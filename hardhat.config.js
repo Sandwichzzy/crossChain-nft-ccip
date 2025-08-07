dae5594f915241bd3aef5ec32ccebd92bf0e78ab
Zhenyu Zhou <787599881@qq.com>
2025-08-07 15:51:24 +0800

    跨链配置

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("hardhat-deploy");
+require("@chainlink/env-enc").config();
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const PRIVATE_KEY_2 = process.env.PRIVATE_KEY_2;
+const PRIVATE_KEY_3 = process.env.PRIVATE_KEY_3;
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+const AMOY_RPC_URL = process.env.AMOY_RPC_URL;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -7,12 +15,29 @@ module.exports = {
   mocha: {
     timeout: 500000,
   },
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY, PRIVATE_KEY_2, PRIVATE_KEY_3],
+      chainId: 11155111,
+      blockConfirmations: 6,
+    },
+    amoy: {
+      url: AMOY_RPC_URL,
+      accounts: [PRIVATE_KEY, PRIVATE_KEY_2, PRIVATE_KEY_3],
+      chainId: 80002,
+      blockConfirmations: 6,
+    },
+  },
   namedAccounts: {
     firstAccount: {
       default: 0,
     },
     secondAccount: {
       default: 1,
+    },
+    thirdAccount: {
+      default: 2,
     },
   },
 };
